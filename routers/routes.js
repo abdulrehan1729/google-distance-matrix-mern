22a5379784724bea529654351ea879a95aabf048
Abdul Rehan <abdulrehan1729@gmail.com>
2020-06-21 00:29:06 +0530

    added gitignore

--- routers/routes.js
+++ routers/routes.js
@@ -1,18 +1,17 @@
-const router = require('express').Router()
-const distance = require('google-distance-matrix');
+const router = require("express").Router();
+const distance = require("google-distance-matrix");
+require("dotenv").config();
 
-router.get('/', (req, res) => {
+router.get("/", (req, res) => {
+  var origins = ["bangalore"];
+  var destinations = ["indore"];
 
-    var origins = ['bangalore'];
-    var destinations = ['indore'];
+  distance.key(process.env.GOOGLE_API_KEY);
 
-    distance.key(process.env.GOOGLE_API_KEY)
-
-    distance.matrix(origins, destinations, function (err, distances) {
-        if (!err)
-            console.log(distances);
-        res.send(distances)
-    })
-})
+  distance.matrix(origins, destinations, function (err, distances) {
+    if (!err) console.log(distances);
+    res.send(distances);
+  });
+});
 
 module.exports = router;
